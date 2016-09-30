63a5f68130c24b5c75b44c70de6a41c5ae7023d3
Ricky Keisling <rkeisling@basecampcodingacademy.org>
2016-09-30 13:49:21 -0500

    Added some js functions

--- sudoku.js
+++ sudoku.js
@@ -1,13 +1,13 @@
 var board = {
   rows: [[1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9],
-          [1, 2, 3, 4, 5, 6, 7, 8, 9]],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9],
+         [1, 2, 3, 4, 5, 6, 7, 8, 9]],
   guesses: [[null, null, null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null, null, null],
@@ -18,6 +18,36 @@ var board = {
             [null, null, null, null, null, null, null, null, null],
             [null, null, null, null, null, null, null, null, null]],
   addGuess: function(guess, position) {
-    this.guesses[position] = guess
+    document.getElementsByName('').innerText = (guess goes here);
+    var input = document.getElementsByName('userInput').value;
+    this.guesses[position] = guess;
+
+  }
+
+  check: function () {
+    this.readGuesses();
+    this.markGuesses();
+  },
+
+  readGuesses: function () {
+    var trs = document.getElementById('board').getElementsByTagName('tr');
+    for (var i = 0; i < trs.length; i++) {
+      var tds = trs[i].getElementsByTagName('td');
+      for (var j = 0; j < tds.length; j++) {
+        var value = tds[j].getElementsByTagName('input')[0].value;
+        this.guesses[i][j] = Number(value);
+      }
+    }
+  },
+
+  markGuesses: function () {
+    var trs = document.getElementById('board').getElementsByTagName('tr');
+    for (var i = 0; i < this.guesses.length; i++) {
+      var tds = trs[i].getElementsByTagName('td');
+      for (var j = 0; i < this.guesses[i].length; j++) {
+        var value = this.guesses[i][j];
+        document.getElementsByTagName('')
+      }
+    }
   }
 }
